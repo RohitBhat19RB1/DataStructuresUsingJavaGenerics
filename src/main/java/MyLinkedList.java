6f317b06ff3aab43d3f7f0cf046ee2baf8f02eaa
RohitBhat19RB1 <rohitbhat16860@gmail.com>
2020-12-03 21:17:28 +0530

    Added:append method to add the data at the last

---
+++ src/main/java/MyLinkedList.java
@@ -0,0 +1,48 @@
+public class MyLinkedList {
+    public static INode head;
+    public static INode tail;
+    public MyLinkedList() {
+        this.head=null;
+        this.tail=null;
+    }
+
+    public void add(INode newNode) {
+        if(this.tail==null) {
+            tail = newNode;
+        }
+        if(this.head==null){
+            this.head=newNode;
+        }else {
+            INode tempNode=this.head;
+            this.head=newNode;
+            this.head.setNext(tempNode);
+        }
+    }
+
+    public void PrintMyNodes(){
+        StringBuffer myNodes=new StringBuffer("MyNodes:");
+        INode tempNode=head;
+        while (tempNode.getNext()!=null){
+            myNodes.append(tempNode.getKey());
+            if(tempNode.equals(tail)) myNodes.append("->");
+            tempNode=tempNode.getNext();
+        }
+        myNodes.append(tempNode.getKey());
+        System.out.println(myNodes);
+    }
+
+    public void append(INode myNode) {
+        if(this.head==null){
+            this.head=myNode;
+        }
+        if(this.tail==null){
+            this.tail=myNode;
+        }else{
+            this.tail.setNext(myNode);
+            this.tail=myNode;
+        }
+    }
+
+
+
+}
---
+++ src/test/java/LinkedListTest.java
@@ -0,0 +1,52 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LinkedListTest {
+
+    @Test
+    public void given3NumbersWhenLinkedShouldPassedLinkedListTest() {
+        MyNode<Integer> MyFirstNode=new MyNode<>(56);
+        MyNode<Integer> MySecondNode=new MyNode<>(30);
+        MyNode<Integer> MyThirdNode=new MyNode<>(70);
+        MyFirstNode.setNext(MySecondNode);
+        MySecondNode.setNext(MyThirdNode);
+        boolean result=MyFirstNode.getNext().equals(MySecondNode)&&
+                MySecondNode.getNext().equals(MyThirdNode);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void given3NumbersWhenAddedToLinkedListShouldBeAddAtTop() {
+        MyNode<Integer> MyFirstNode=new MyNode<>(70);
+        MyNode<Integer> MySecondNode=new MyNode<>(30);
+        MyNode<Integer> MyThirdNode=new MyNode<>(56);
+        MyLinkedList linkedList=new MyLinkedList();
+        linkedList.add(MyFirstNode);
+        linkedList.add(MySecondNode);
+        linkedList.add(MyThirdNode);
+        boolean result=MyLinkedList.head.equals(MyThirdNode)&&
+                MyLinkedList.head.getNext().equals(MySecondNode)&&
+                MyLinkedList.tail.equals(MyFirstNode);
+        Assert.assertTrue(result);
+        linkedList.PrintMyNodes();
+    }
+
+    @Test
+    public void given3NumbersWhenAddedToLinkedListShouldBeaddAtLast() {
+        MyNode<Integer> MyFirstNode=new MyNode<>(56);
+        MyNode<Integer> MySecondNode=new MyNode<>(30);
+        MyNode<Integer> MyThirdNode=new MyNode<>(70);
+        MyLinkedList linkedList=new MyLinkedList();
+        linkedList.add(MyFirstNode);
+        linkedList.append(MySecondNode);
+        linkedList.append(MyThirdNode);
+        linkedList.PrintMyNodes();
+        boolean result=MyLinkedList.head.equals(MyFirstNode)&&
+                MyLinkedList.head.getNext().equals(MySecondNode)&&
+                MySecondNode.getNext().equals(MyThirdNode);
+        Assert.assertTrue(result);
+
+    }
+
+
+}
